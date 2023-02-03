c57d5a7a2b0978a0091b240c21b82b7b51146f15
Shahriyar Zaman Ridoy <104163801+shahriyar-zaman@users.noreply.github.com>
2023-02-03 09:04:51 +0600

    Add files via upload

---
+++ B.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <iomanip>
+
+using namespace std;
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define mems(x, n) memset(x, n, sizeof(x))
+#define readfile freopen("input.txt", "r", stdin)
+#define debug(x) cout << #x << " is " << x << endl
+const int dx[] = {-1, 1, 0, 0, -1, -1, 1, 1};
+const int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
+const int N = 1e6+10;
+
+void testCase() {
+    
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    while(tc--)
+        testCase();
+    return 0;
+}
